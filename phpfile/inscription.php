9f5e9a56431bbd187c7cb76f315b76ead3684d6a
Titouan <117993648+TitouanDor@users.noreply.github.com>
2025-04-16 09:39:22 +0200

    modification inscription.php

--- phpfile/inscription.php
+++ phpfile/inscription.php
@@ -1,11 +1,11 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=virtual_trader;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=localhost;dbname=..\virtual_trader;charset=utf8', 'root', '');
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $email = $_POST['e-mail'];
-$password = $_POST['mdp'];
-$req = $bdd->prepare("INSERT INTO users(nom, prenom, email,mot_de_passe,argent) VALUES (?,?,?,?,?);");
-$req->execute([$prenom,$nom,$email,$password,1000]);
+$password = PASSWORD($_POST['mdp']);
+$req = $bdd->prepare("INSERT INTO users(email, mdp, nom,prenom,argent) VALUES (?,?,?,?,?);");
+$req->execute([$email,$password,$nom,$prenom]);
 header('Location: ..\index.html');
 exit();
 ?>
