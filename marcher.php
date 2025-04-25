5b6e2fffbb3dbf9c97c37b18fe067af6fe5de53c
Titouan <117993648+TitouanDor@users.noreply.github.com>
2025-04-25 12:05:44 +0200

    ajout page marcher
    affichage de toutes les actions actuelles

--- marcher.php
+++ marcher.php
@@ -5,8 +5,35 @@
     <title>Marche</title>
 </head>
 <body>
+<?php
+// Connect to the database and execute the request
+$bdd = new PDO('mysql:host=localhost;dbname=virtual_trader;charset=utf8', 'root', '');
+$req = $bdd->prepare("SELECT nom, description, prix FROM actions;");
+$req->execute();
+
+?>
 <div>
-    <!--Permet de voir le marche (toutes les actions)-->
+    <table>
+        <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Date de naissance</th>
+        </tr>
+        <?php
+        while($data = $req->fetch())
+        {
+            ?>
+            <tr>
+                <td><?php echo $data["nom"]; ?></td>
+                <td><?php echo $data["description"]; ?></td>
+                <td><?php echo $data["prix"]; ?></td>
+            </tr>
+            <?php
+
+        }
+        ?>
+    </table>
+
 </div>
 </body>
 </html>
