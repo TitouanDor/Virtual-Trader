220be20990c9427fb6edee7015b119bd6be1b8f1
androtim2006 <androtim2006@gmail.com>
2025-04-26 21:20:59 +0000

    ....

--- profil.php
+++ profil.php
@@ -1,4 +1,5 @@
 <?php
+include "actionDuTemps.php";
 session_start();
 // Vérification si l'utilisateur est connecté
 if (!isset($_SESSION['id'])) {
@@ -20,68 +21,68 @@ $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $gameStateReq = $bdd->prepare("SELECT * FROM game_state");
     $gameStateReq->execute();
     $gameState = $gameStateReq->fetch();
-    if ($gameState) {
-        $lastUpdate = new DateTime($gameState['last_update']);
-        $currentTime = new DateTime();
+//    if ($gameState) {
+//        $lastUpdate = new DateTime($gameState['last_update']);
+//        $currentTime = new DateTime();
 
-        // Check if the game has to be updated
-         if ($currentTime->diff($lastUpdate)->i >= 1) {
+//        // Check if the game has to be updated
+//         if ($currentTime->diff($lastUpdate)->i >= 1) {
 
-            $currentMonth = $gameState['current_month'];
-           $currentYear = $gameState['current_year'];
-           $currentMonth++;
-           if ($currentMonth > 12) {
-                $currentMonth = 1;
-                $currentYear++;
-            }
+//            $currentMonth = $gameState['current_month'];
+//           $currentYear = $gameState['current_year'];
+//           $currentMonth++;
+//           if ($currentMonth > 12) {
+//                $currentMonth = 1;
+//                $currentYear++;
+//            }
 
-            $updateDateReq = $bdd->prepare("UPDATE game_state SET current_month = ?, current_year = ?, last_update = ?");
-            $updateDateReq->execute([$currentMonth, $currentYear, $currentTime->format('Y-m-d H:i:s')]);
-            //give dividends
-           $dividendReq = $bdd->prepare("SELECT joueur.id, joueur.argent, actions.dividende FROM joueur JOIN portefeuille ON joueur.id = portefeuille.joueur_id JOIN actions ON portefeuille.action_id = actions.id WHERE actions.date_dividende = ?");
-           $dividendReq->execute([$currentMonth]);
-           $players = $dividendReq->fetchAll();
-           foreach($players as $player){
-               $new_money = floatval($player['argent']) + floatval($player['dividende']);
-               $updateMoney = $bdd->prepare("UPDATE joueur SET argent = ? WHERE id = ?");
-               $updateMoney->execute([$new_money, $player['id']]);
-           }
-           // Update the price of stocks
-           $stocksReq = $bdd->prepare("SELECT id, prix FROM actions");
-           $stocksReq->execute();
-           $stocks = $stocksReq->fetchAll();
-           foreach ($stocks as $stock) {
-               $lastMonth = $currentMonth == 1 ? 12 : $currentMonth - 1;
-               $lastYear = $currentMonth == 1 ? $currentYear - 1 : $currentYear;
+//            $updateDateReq = $bdd->prepare("UPDATE game_state SET current_month = ?, current_year = ?, last_update = ?");
+//            $updateDateReq->execute([$currentMonth, $currentYear, $currentTime->format('Y-m-d H:i:s')]);
+//            //give dividends
+//           $dividendReq = $bdd->prepare("SELECT joueur.id, joueur.argent, actions.dividende FROM joueur JOIN portefeuille ON joueur.id = portefeuille.joueur_id JOIN actions ON portefeuille.action_id = actions.id WHERE actions.date_dividende = ?");
+//           $dividendReq->execute([$currentMonth]);
+//           $players = $dividendReq->fetchAll();
+//           foreach($players as $player){
+//               $new_money = floatval($player['argent']) + floatval($player['dividende']);
+//               $updateMoney = $bdd->prepare("UPDATE joueur SET argent = ? WHERE id = ?");
+//               $updateMoney->execute([$new_money, $player['id']]);
+//           }
+//           // Update the price of stocks
+//           $stocksReq = $bdd->prepare("SELECT id, prix FROM actions");
+//           $stocksReq->execute();
+//           $stocks = $stocksReq->fetchAll();
+//           foreach ($stocks as $stock) {
+//               $lastMonth = $currentMonth == 1 ? 12 : $currentMonth - 1;
+//               $lastYear = $currentMonth == 1 ? $currentYear - 1 : $currentYear;
            
-                // Check if we have the last price
-                $lastPriceExistReq = $bdd->prepare("SELECT COUNT(*) AS count FROM cours_marche WHERE action_id = ? AND game_month = ? AND game_year = ?");
-                $lastPriceExistReq->execute([$stock['id'], $lastMonth, $lastYear]);
-                $lastPriceExist = $lastPriceExistReq->fetch();
+//                // Check if we have the last price
+//                $lastPriceExistReq = $bdd->prepare("SELECT COUNT(*) AS count FROM cours_marche WHERE action_id = ? AND game_month = ? AND game_year = ?");
+//                $lastPriceExistReq->execute([$stock['id'], $lastMonth, $lastYear]);
+//                $lastPriceExist = $lastPriceExistReq->fetch();
 
-                if ($lastPriceExist['count'] == 0) {
-                    $percent = 0;
-                } else {
-                    $lastPriceReq = $bdd->prepare("SELECT valeur_action FROM cours_marche WHERE action_id = ? AND game_month = ? AND game_year = ?");
-                    $lastPriceReq->execute([$stock['id'], $lastMonth, $lastYear]);
-                    $lastPrice = $lastPriceReq->fetch();
+//                if ($lastPriceExist['count'] == 0) {
+//                    $percent = 0;
+//                } else {
+//                    $lastPriceReq = $bdd->prepare("SELECT valeur_action FROM cours_marche WHERE action_id = ? AND game_month = ? AND game_year = ?");
+//                    $lastPriceReq->execute([$stock['id'], $lastMonth, $lastYear]);
+//                    $lastPrice = $lastPriceReq->fetch();
 
-                    $percent = 0;
-                    if ($lastPrice !== false) {
-                        $percent = $lastPrice['valeur_action'] == 0 ? 0 : ((floatval($stock['prix']) - floatval($lastPrice['valeur_action'])) / floatval($lastPrice['valeur_action'])) * 100;
-                    }
-                    $percent += rand(-3, 3);
-                    $percent = max(-10, min(10, $percent));
-                    $newPrice = floatval($stock['prix']) + (floatval($stock['prix']) * ($percent / 100));
-                }
-                $newPrice = max(1, $newPrice);
-                $newPriceReq = $bdd->prepare("UPDATE actions SET prix = ? WHERE id = ?");
-                $newPriceReq->execute([$newPrice, $stock['id']]);
-                $addCoursReq = $bdd->prepare("INSERT INTO cours_marche (action_id, game_month, game_year, valeur_action) VALUES (?, ?, ?, ?)");
-                $addCoursReq->execute([$stock['id'], $currentMonth, $currentYear, $newPrice]);
-           }
-        }        
-    }
+//                    $percent = 0;
+//                    if ($lastPrice !== false) {
+//                        $percent = $lastPrice['valeur_action'] == 0 ? 0 : ((floatval($stock['prix']) - floatval($lastPrice['valeur_action'])) / floatval($lastPrice['valeur_action'])) * 100;
+//                    }
+//                    $percent += rand(-3, 3);
+//                    $percent = max(-10, min(10, $percent));
+//                    $newPrice = floatval($stock['prix']) + (floatval($stock['prix']) * ($percent / 100));
+//                }
+//                $newPrice = max(1, $newPrice);
+//                $newPriceReq = $bdd->prepare("UPDATE actions SET prix = ? WHERE id = ?");
+//                $newPriceReq->execute([$newPrice, $stock['id']]);
+//                $addCoursReq = $bdd->prepare("INSERT INTO cours_marche (action_id, game_month, game_year, valeur_action) VALUES (?, ?, ?, ?)");
+//                $addCoursReq->execute([$stock['id'], $currentMonth, $currentYear, $newPrice]);
+//           }
+//        }        
+//    }
 
 $userReq = $bdd->prepare("SELECT nom, prenom, email, argent FROM joueur WHERE id = ?"); // Récupération des informations de l'utilisateur
 $userReq->execute([$_SESSION['id']]);
