cf9bbde2ed22df2f5b698b234698603a78bf84d9
Androtim <androtim2006@gmail.com>
2025-04-26 02:36:08 +0200

    change index

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
+<a href="leaderboard.php">Leaderboard</a>
+<br>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSSFile\index.css">
+    <title>Virtual-trader</title>
+</head>
+<body>
+
+<?php
+if (isset($_SESSION['error'])) {
+    echo "<p>";
+    echo str_replace("\n", "<br>", $_SESSION['error']);
+    echo "</p>";
+    unset($_SESSION['error']);
+}
+
+if (isset($_SESSION['success'])) {
+    echo "<p>";
+    echo str_replace("\n", "<br>", $_SESSION['success']);
+    echo "</p>";
+    unset($_SESSION['success']);
+}
+if(!isset($_SESSION['id'])): ?>
+    <div name="box_connection" class="box_connection">
+        <p>Connection à Virtual-trader</p>
+        <form action="connectionScript.php" method="post">
+            <div class="group-from">
+                <label for="email">
+                    <input type="text" name="email" id="email" placeholder="E-mail" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>">
+                </label>
+            </div>
+            <div class="group-from">
+                <label for="password">
+                    <input type="password" name="password" id="password" placeholder="mot de passe">
+                </label>
+            </div>
+
+            <div class="group-from">
+                <label>
+                    <input type="submit" value="Se connecter">
+                </label>
+            </div>
+        </form>
+
+        <p><a href="inscription.php">Inscription</a></p>
+    </div>
+<?php endif; ?>
+
+</body>
+</html>
+
+
 <?php
 session_start();
 
@@ -131,62 +187,3 @@ if ($currentTime->diff($lastUpdate)->i >= 1) {
 }
 
 ?>
-
-<a href="leaderboard.php">Leaderboard</a>
-<br>
-
-<?php
-
-?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="CSSFile\index.css">
-    <title>Virtual-trader</title>
-</head>
-<body>
-
-    <?php 
-    if (isset($_SESSION['error'])) {
-        echo "<p>";
-        echo str_replace("\n", "<br>", $_SESSION['error']);
-        echo "</p>";
-        unset($_SESSION['error']);
-    }
-    
-    if (isset($_SESSION['success'])) {
-        echo "<p>";
-        echo str_replace("\n", "<br>", $_SESSION['success']);
-        echo "</p>";
-        unset($_SESSION['success']);
-    }
-    if(!isset($_SESSION['id'])): ?>
-        <div name="box_connection" class="box_connection">
-            <p>Connection à Virtual-trader</p>
-            <form action="connectionScript.php" method="post">
-                <div class="group-from">
-                    <label for="email">
-                        <input type="text" name="email" id="email" placeholder="E-mail" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '' ?>">
-                    </label>
-                </div>
-                <div class="group-from">
-                    <label for="password">
-                        <input type="password" name="password" id="password" placeholder="mot de passe">
-                    </label>
-                </div>
-    
-                <div class="group-from">
-                    <label>
-                        <input type="submit" value="Se connecter">
-                    </label>
-                </div>
-        </form>
-
-        <p><a href="inscription.php">Inscription</a></p>
-    </div>
-<?php endif; ?>
-
-</body>
-</html>
