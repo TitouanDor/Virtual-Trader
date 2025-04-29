c52c6e0b9c6fcb89b4663085f16e66b349857fb5
Titouan <117993648+TitouanDor@users.noreply.github.com>
2025-04-29 14:38:33 +0200

    add classement.css

--- classement.php
+++ classement.php
@@ -32,6 +32,7 @@ if ($from === 'index') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSSFile/classement.css">
     <title>Leaderboard</title>
 </head>
 <body>
