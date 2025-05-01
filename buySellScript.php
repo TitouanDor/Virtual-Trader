73b7379a75584e8ac85f1d5dd5a5638016a12905
Titouan <117993648+TitouanDor@users.noreply.github.com>
2025-05-01 11:05:24 +0200

    Update buySellScript.php
    verifier si LE joueur connecter a déjà acheter une action et non que l'action a déjà ete acheter par UN joueur

--- buySellScript.php
+++ buySellScript.php
@@ -16,87 +16,86 @@ try {
     exit();
 }
 
-    $action_id = $_POST['action_id'];
-    $quantite = $_POST['quantity'];
-    $action = $_POST['action'];
+$action_id = $_POST['action_id'];
+$quantite = $_POST['quantity'];
+$action = $_POST['action'];
 
 
-    $achat_venteReq = $bdd->prepare("SELECT * FROM actions WHERE id = ?");
-    $achat_venteReq->execute([$action_id]);
-    $achat_vente = $achat_venteReq->fetch();
+$achat_venteReq = $bdd->prepare("SELECT * FROM actions WHERE id = ?");
+$achat_venteReq->execute([$action_id]);
+$achat_vente = $achat_venteReq->fetch();
 
-    $arg_joueurReq = $bdd->prepare("SELECT argent FROM joueur WHERE id = ?");
-    $arg_joueurReq->execute([$_SESSION['id']]);
-    $arg_joueur = $arg_joueurReq->fetch();
+$arg_joueurReq = $bdd->prepare("SELECT argent FROM joueur WHERE id = ?");
+$arg_joueurReq->execute([$_SESSION['id']]);
+$arg_joueur = $arg_joueurReq->fetch();
 
-    $prix_total = $achat_vente['prix'] * $quantite;
+$prix_total = $achat_vente['prix'] * $quantite;
 
-    // Recup mois et annee
-    $gameStateReq = $bdd->query("SELECT current_month, current_year FROM game_state");
-    $gameState = $gameStateReq->fetch();
-    $currentMonth = $gameState['current_month'];
-    $currentYear = $gameState['current_year'];
+// Recup mois et annee
+$gameStateReq = $bdd->query("SELECT current_month, current_year FROM game_state");
+$gameState = $gameStateReq->fetch();
+$currentMonth = $gameState['current_month'];
+$currentYear = $gameState['current_year'];
 
-    $nouv_arg_joueur = $arg_joueur['argent'];
+$nouv_arg_joueur = $arg_joueur['argent'];
 
 //Recup portefeuille Joueur
-$PortefeuilleReq = $bdd->prepare("SELECT * FROM portefeuille WHERE action_id = ?");
-$PortefeuilleReq->execute([$action_id]);
+$PortefeuilleReq = $bdd->prepare("SELECT * FROM portefeuille WHERE action_id = ? and joueur_id=?");
+$PortefeuilleReq->execute([$action_id, $_SESSION['id']]);
 $Portefeuille = $PortefeuilleReq->fetch();
 
 
-    if ($action == 'Acheter') {
+if ($action == 'Acheter') {
 
-        $nouv_arg_joueur = $arg_joueur['argent'] - $prix_total;
+    $nouv_arg_joueur = $arg_joueur['argent'] - $prix_total;
 
-        //Verif si joueur assez argent pour acheter actions
-        if($nouv_arg_joueur < 1000.00) {
-            echo "Vous n'avez pas assez d'argent";
-            exit();
+    //Verif si joueur assez argent pour acheter actions
+    if($nouv_arg_joueur < 1000.00) {
+        echo "Vous n'avez pas assez d'argent";
+        exit();
+    }
+
+    //Regarder si le portefeuille contient déjà l'action ou non et update en conséquence
+    if($Portefeuille) {
+        $nouvQtitPortef = $Portefeuille['quantite'] + $quantite;
+        $PortefeuilleMAJ = $bdd->prepare("UPDATE portefeuille SET quantite = ? WHERE action_id = ?");
+        $PortefeuilleMAJ->execute([$nouvQtitPortef, $action_id]);
+    }
+    else {
+        $PortefeuilleReq = $bdd->prepare("INSERT INTO portefeuille (joueur_id, action_id, quantite, prix_achat) VALUES (?, ?, ?, ?)");
+        $PortefeuilleReq->execute([$_SESSION['id'], $action_id, $quantite, $prix_total]);
+    }
+
+}
+if ($action == 'Vendre') {
+    $nouv_arg_joueur = $arg_joueur['argent'] + $prix_total;
+
+    //Regarder si le portefeuille contient déjà l'action ou non et update en conséquence
+    if($Portefeuille) {
+        $nouvQtitPortef = $Portefeuille['quantite'] - $quantite;
+        if($nouvQtitPortef <= 0) {
+            $PortefeuilleMAJ = $bdd->prepare("DELETE FROM portefeuille WHERE action_id = ?");
+            $PortefeuilleMAJ->execute([$action_id]);
         }
-
-        //Regarder si le portefeuille contient déjà l'action ou non et update en conséquence
-        if($Portefeuille) {
-            $nouvQtitPortef = $Portefeuille['quantite'] + $quantite;
+        else {
             $PortefeuilleMAJ = $bdd->prepare("UPDATE portefeuille SET quantite = ? WHERE action_id = ?");
             $PortefeuilleMAJ->execute([$nouvQtitPortef, $action_id]);
         }
-        else {
-            $PortefeuilleReq = $bdd->prepare("INSERT INTO portefeuille (joueur_id, action_id, quantite, prix_achat) VALUES (?, ?, ?, ?)");
-            $PortefeuilleReq->execute([$_SESSION['id'], $action_id, $quantite, $prix_total]);
-        }
-
     }
-    if ($action == 'Vendre') {
-        $nouv_arg_joueur = $arg_joueur['argent'] + $prix_total;
-
-        //Regarder si le portefeuille contient déjà l'action ou non et update en conséquence
-        if($Portefeuille) {
-            $nouvQtitPortef = $Portefeuille['quantite'] - $quantite;
-            if($nouvQtitPortef <= 0) {
-                $PortefeuilleMAJ = $bdd->prepare("DELETE FROM portefeuille WHERE action_id = ?");
-                $PortefeuilleMAJ->execute([$action_id]);
-            }
-            else {
-                $PortefeuilleMAJ = $bdd->prepare("UPDATE portefeuille SET quantite = ? WHERE action_id = ?");
-                $PortefeuilleMAJ->execute([$nouvQtitPortef, $action_id]);
-            }
-        }
-        else {
-            echo "Vous n'avez pas assez d'action de cette entreprise";
-            exit();
-        }
-
+    else {
+        echo "Vous n'avez pas assez d'action de cette entreprise";
+        exit();
     }
 
-    //MAJ de l'argent du joueur
-    $MAJ_Joueur = $bdd->prepare("UPDATE joueur SET argent = ? WHERE id = ?");
-    $MAJ_Joueur->execute([$nouv_arg_joueur, $_SESSION['id']]);
+}
 
-    //MAJ de l'historique du jeu
-    $HistoriqueReq = $bdd->prepare("INSERT INTO historique (action_id, joueur_id, prix, nature, quantite, game_month, game_year) VALUES (?, ?, ?, ?, ?, ?, ?) ");
-    $HistoriqueReq->execute([$action_id, $_SESSION['id'], $prix_total, $action, $quantite, $currentMonth, $currentYear]);
+//MAJ de l'argent du joueur
+$MAJ_Joueur = $bdd->prepare("UPDATE joueur SET argent = ? WHERE id = ?");
+$MAJ_Joueur->execute([$nouv_arg_joueur, $_SESSION['id']]);
 
+//MAJ de l'historique du jeu
+$HistoriqueReq = $bdd->prepare("INSERT INTO historique (action_id, joueur_id, prix, nature, quantite, game_month, game_year) VALUES (?, ?, ?, ?, ?, ?, ?) ");
+$HistoriqueReq->execute([$action_id, $_SESSION['id'], $prix_total, $action, $quantite, $currentMonth, $currentYear]);
 
 
 
