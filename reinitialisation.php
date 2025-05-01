70c3a5d8bd060f2d777afb9bdb160bf59f5462b1
Titouan <117993648+TitouanDor@users.noreply.github.com>
2025-05-01 11:19:45 +0200

    Update reinitialisation.php
    ajout entete

--- reinitialisation.php
+++ reinitialisation.php
@@ -2,5 +2,10 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+<head>
+    <title>Réinitialisation</title>
+    <link rel="stylesheet" href="CSSFile/general.css">
+    <link rel="stylesheet" href="CSSFile/reinitialisation.css">
+</head>
 
 </html>
